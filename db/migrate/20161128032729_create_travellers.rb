135a74191776cc63333a30e2eb6eaa69a53f02e9
mariomaia77 <mamsm77@gmail.com>
2016-11-28 14:40:53 +1100

    first commit

---
+++ db/migrate/20161128032729_create_travellers.rb
@@ -0,0 +1,23 @@
+class CreateTravellers < ActiveRecord::Migration
+  def change
+    create_table :travellers do |t|
+      t.text :first_name
+      t.text :last_name
+      t.string :email
+      t.text :image
+      t.date :dob
+      t.text :password_digest
+      t.boolean :admin
+      t.string :phone
+      t.text :city
+      t.text :country
+      t.float :lat
+      t.float :long
+      t.text :fav_winery
+      t.text :fav_region
+      t.text :fav_variety
+
+      t.timestamps null: false
+    end
+  end
+end
